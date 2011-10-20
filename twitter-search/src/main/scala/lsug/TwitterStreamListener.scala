aa4ecfe5f4f8df5f1b5d0a13e15f3903091c6688
Paweł Krupinski <pawel@krupinski.com>
2011-10-20 20:27:21 +0100

    first working version

--- twitter-search/src/main/scala/lsug/TwitterStreamListener.scala
+++ twitter-search/src/main/scala/lsug/TwitterStreamListener.scala
@@ -1,12 +1,20 @@
 package lsug
 
 import twitter4j.conf.{Configuration, ConfigurationBuilder}
-import twitter4j.{Twitter, TwitterFactory}
+import twitter4j.{Query, Twitter, TwitterFactory}
+import scala.collection.JavaConversions._
 
 class TwitterStreamListener {
   val cb = new ConfigurationBuilder();
   val build: Configuration = cb.setDebugEnabled(true).setUser("lsugoct").setPassword("Rfsd5819").build()
   val twitterFactory: TwitterFactory = new TwitterFactory(build)
   val twitter: Twitter = twitterFactory.getInstance()
+  val res = twitter.search(new Query("IBM"))
+  res.getTweets.foreach (println)
+}
 
+object TwitterStream{
+  def main(args:Array[String]){
+    new TwitterStreamListener()
+  }
 }
