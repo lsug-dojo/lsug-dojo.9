7663a17a77063cdeed0634314a9cc6ac6bdcc294
Paweł Krupinski <pawel@krupinski.com>
2011-10-20 20:35:23 +0100

    Formatted output

--- twitter-search/src/main/scala/lsug/TwitterStreamListener.scala
+++ twitter-search/src/main/scala/lsug/TwitterStreamListener.scala
@@ -1,16 +1,23 @@
 package lsug
 
 import twitter4j.conf.{Configuration, ConfigurationBuilder}
-import twitter4j.{Query, Twitter, TwitterFactory}
 import scala.collection.JavaConversions._
+import twitter4j.{Tweet, Query, Twitter, TwitterFactory}
+import java.text.SimpleDateFormat
 
 class TwitterStreamListener {
   val cb = new ConfigurationBuilder();
   val build: Configuration = cb.setDebugEnabled(true).setUser("lsugoct").setPassword("Rfsd5819").build()
   val twitterFactory: TwitterFactory = new TwitterFactory(build)
   val twitter: Twitter = twitterFactory.getInstance()
+  val df = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss")
   val res = twitter.search(new Query("IBM"))
-  res.getTweets.foreach (println)
+  res.getTweets.foreach ( (tweet: Tweet) => {
+    val time = df.format(tweet.getCreatedAt)
+    val author = tweet.getFromUser
+    val text = tweet.getText
+    println("%s@%s %s".format(time, author, text))
+  })
 }
 
 object TwitterStream{
