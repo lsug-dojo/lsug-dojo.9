6cd46e6bfa1a36bec9f12dc6395c9ba500449144
xavji <xavier.lemaignen@gmail.com>
2011-12-01 22:22:01 +0000

    initial commit of email notification project

--- project/Build.scala
+++ project/Build.scala
@@ -2,12 +2,17 @@ import sbt._
 import Keys._
 
 object TwitterBuild extends Build {
+
     lazy val twitterSearch = Project(id = "twitter-search",
                            base = file("twitter-search"))
 
     lazy val stocks = Project(id = "stocks",
                            base = file("stocks")) dependsOn(twitterSearch)
 
-  lazy val friends = Project(id = "friends",
+    lazy val friends = Project(id = "friends",
 			   base = file("friends")) dependsOn(twitterSearch)
+
+    lazy val emailNotification = Project(id = "email-notification",
+                           base = file("email-notification")) dependsOn(twitterSearch)
+
 }
